59e37b8f2501f1e0dd15acdf9c40a2c6f0337a08
Ihar Tsalka <git@tsobako.me>
2024-02-18 19:35:26 +0300

    Fix per review

---
+++ lib/echo_server.ex
@@ -0,0 +1,2 @@
+defmodule EchoServer do
+end
--- test/calculator_test.exs
+++ test/calculator_test.exs
@@ -2,7 +2,17 @@ defmodule CalculatorTest do
   use ExUnit.Case
   doctest Calculator
 
-  test "perform combined operation" do
-    assert Calculator.div(Calculator.add(1230, 3214), 11) == 404
+  test "addition does not fail when both numbers are large" do
+    assert Calculator.add(10 ** 1024, 10 ** 1024) == 2 * 10 ** 1024
+  end
+
+  test "multiplication does not fail when both numbers are large" do
+    assert Calculator.mul(10 ** 1024, 10 ** 1024) == 10 ** 2048
+  end
+
+  test "multiplication fails with large inverse numbers" do
+    assert_raise ArithmeticError, fn ->
+      Calculator.mul(10 ** 1_000_000, 1 / 10 ** 1_000_000)
+    end
   end
 end
